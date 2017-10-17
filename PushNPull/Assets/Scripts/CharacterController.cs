afbb82cfafe20c04169a2169c3bc1c23a5bc2c89
Antoine Berthiaume Dutil <antoineberthiaumedutil@gmail.com>
2017-10-17 00:55:57 -0400

    First Push

---
+++ PushNPull/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    Vector3 screenPoint;
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        curPosition.x = Mathf.Clamp(curPosition.x, -2.5f, 2.5f);
+        curPosition.y = -4.5f;
+        transform.position = curPosition;
+    }
+}
